9736ea35e9688368f9c70636cf131eee7eed306c
Brendan Long <bren.c.long@gmail.com>
2017-03-27 14:02:30 +0100

    Added blog to navbar

--- home_app/urls.py
+++ home_app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import home, about, packages, listen, faq, contact, login
+from .views import home, about, packages, listen, faq, contact, blog, login
 
 urlpatterns = [
     url(r'^$', home, name="index"),
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^listen', listen, name="listen"),
     url(r'^faq', faq, name="faq"),
     url(r'^contact', contact, name="contact"),
+    url(r'^blog', blog, name="blog"),
     url(r'^login', login, name="login"),
 ]
 
--- home_app/views.py
+++ home_app/views.py
@@ -27,5 +27,9 @@ def contact(request):
     return render(request, 'contact.html')
 
 
+def blog(request):
+    return render(request, 'blog.html')
+
+
 def login(request):
     return render(request, 'login.html')
