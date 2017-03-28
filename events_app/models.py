c93c1452a94dfdea6f1704d2594a5e04a63b3192
Brendan Long <bren.c.long@gmail.com>
2017-03-28 15:33:49 +0100

    modified events app

--- events_app/models.py
+++ events_app/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+
+    owner = models.ForeignKey('auth.User')
+    name = models.CharField(max_length=200)
+    date_time = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
