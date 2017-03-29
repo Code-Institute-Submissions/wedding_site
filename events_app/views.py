44803028a4c58591bcd8256e25ead29edca49489
Brendan Long <bren.c.long@gmail.com>
2017-03-29 13:03:02 +0100

    added not authorised to view event functionality & fixed url redirecting on login

--- events_app/views.py
+++ events_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Event
+
 # Create your views here.
+
 
 def event(request, id):
     event = get_object_or_404(Event, pk=id)
@@ -8,3 +10,9 @@ def event(request, id):
     if request.user == event.owner:
         return render(request, 'event_details.html', {'event': event})
     else:
+        return render(request, 'not_auth.html')
+
+
+def userEvent(request):
+    event = Event.objects.get(owner=request.user)
+    return render(request, 'event_details.html', {'event': event})
