138a5d9f78fee0cbabfcf5870a7a5ff818781c67
Brendan Long <bren.c.long@gmail.com>
2017-05-12 14:50:48 +0100

    Fixed deposit payment enabled and working

--- events_app/urls.py
+++ events_app/urls.py
@@ -3,4 +3,5 @@ import views
 
 urlpatterns = [
     url(r'^wedding$', views.userevent, name='user_event'),
+    url(r'^charge$', views.charge, name='charge'),
 ]
--- events_app/views.py
+++ events_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Event
 from django.db.models.base import ObjectDoesNotExist
+import stripe
+from stream3_project import settings
 
 # Create your views here.
 
@@ -12,3 +14,24 @@ def userevent(request):
 
     except ObjectDoesNotExist:
         return render(request, 'no_event.html')
+
+
+def charge(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    token = request.POST['stripeToken']
+    email = request.POST['stripeEmail']
+    amount = 10000
+
+    customer = stripe.Customer.create(
+        email=email,
+        card=token
+    )
+
+    stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='eur',
+        description='Deposit Payment'
+    )
+
+    return render(request, 'charge.html')
