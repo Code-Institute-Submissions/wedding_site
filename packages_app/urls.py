9726e3af4c54a8db6c3f6e88412605a17a78655f
Brendan Long <bren.c.long@gmail.com>
2017-05-16 17:18:43 +0100

    code cleanup

--- packages_app/urls.py
+++ packages_app/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url
-from .views import packages, vocalist, harpist, pianist, ceremony, reception
+from .views import packages, ceremony, reception
 
 urlpatterns = [
     url(r'^$', packages, name="packages"),
-    url(r'^vocalist', vocalist, name="vocalist"),
-    url(r'^harpist', harpist, name="harpist"),
-    url(r'^pianist', pianist, name="pianist"),
     url(r'^ceremony', ceremony, name="ceremony"),
     url(r'^reception', reception, name="reception"),
 ]
